f2bdcadd448bbdf425328c6e9fca29bae79343a5
Christopher Svanefalk <christopher.svanefalk@gmail.com>
2013-10-28 08:51:51 +0100

    Eclipse migration.

---
+++ src/main/java/com/csvanefalk/GeoJSONObject.java
@@ -0,0 +1,14 @@
+package com.csvanefalk;
+
+/**
+ * Represents a GeoJSON object.
+ * 
+ * http://geojson.org/geojson-spec.html
+ * 
+ * @author christopher
+ * 
+ */
+public class GeoJSONObject {
+
+	
+}
---
+++ src/main/java/com/csvanefalk/GeoJSONType.java
@@ -0,0 +1,8 @@
+package com.csvanefalk;
+
+public class GeoJSONType {
+/*
+	public static String POINT = "Point";
+	public static String MULTIPOINT = "MultiPoint" "LineString", "MultiLineString", "Polygon", "MultiPolygon", "GeometryCollection", "Feature", or "FeatureCollection;;;;
+	*/
+}
